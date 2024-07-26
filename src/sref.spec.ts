c370497188598500d65f1a4a5c611f36053e0569
Ade Novid <adenvt@gmail.com>
2024-07-26 22:06:01 +0700

    test: refactor test case

--- src/sref.spec.ts
+++ src/sref.spec.ts
@@ -1,58 +1,81 @@
 import {
   vi,
   it,
+  describe,
   expect,
 } from 'vitest'
 import sRef from './sref'
 import { delay } from 'nanodelay'
 
-it('should able to set & get value', () => {
-  const isLoading = sRef(false)
+describe('.watch()', () => {
+  it('should able value changed', () => {
+    const count = sRef(0)
+    const log   = vi.fn()
 
-  isLoading.value = true
+    count.watch((value, oldValue) => {
+      log(value, oldValue, count.value)
+    })
 
-  expect(isLoading.value).toBe(true)
+    count.value = 5
+
+    expect(log).toBeCalledTimes(1)
+    expect(log).toBeCalledWith(5, 0, 5)
+  })
+
+  it('should able to unwatch', () => {
+    const count = sRef(0)
+    const log   = vi.fn()
+
+    const unWatch = count.watch((value, oldValue) => {
+      log(value, oldValue, count.value)
+    })
+
+    unWatch()
+
+    count.value = 5
+
+    expect(log).not.toBeCalled()
+  })
+
+  it('should emit changed if immidiate set to true', () => {
+    const count = sRef(0)
+    const log   = vi.fn()
+
+    count.watch((value, oldValue) => {
+      log(value, oldValue, count.value)
+    }, { immediate: true })
+
+    expect(log).toBeCalled()
+    expect(log).toBeCalledWith(0, undefined, 0)
+  })
 })
 
-it('should able watch & unwatch value changed', () => {
-  const isLoading = sRef(false)
-  const onChange  = vi.fn()
-  const unWatch   = isLoading.watch(onChange)
+describe('.toBe()', () => {
+  it('should resolve promise when value match expected value', async () => {
+    const count = sRef(0)
+    const match = sRef(false)
 
-  isLoading.value = true
+    count.toBe(2).then(() => { match.value = true })
+    await delay(0)
 
-  expect(isLoading.value).toBe(true)
-  expect(onChange).toBeCalledTimes(1)
-  expect(onChange).toBeCalledWith(true, false)
+    count.value++
+    await delay(0)
 
-  unWatch()
+    expect(match.value).toBe(false)
 
-  isLoading.value = false
+    count.value++
+    await delay(0)
 
-  expect(isLoading.value).toBe(false)
-  expect(onChange).toBeCalledTimes(1)
-})
+    expect(match.value).toBe(true)
+  })
 
-it('should able to waiting for value change toBe', async () => {
-  const count = sRef(0)
+  it('should resolve immidiately if current value match expected value', async () => {
+    const count = sRef(5)
+    const match = sRef(false)
 
-  let done = false
+    count.toBe(5).then(() => { match.value = true })
+    await delay(0)
 
-  count.toBe(3).then(() => { done = true })
-  await delay(0)
-
-  count.value++
-  await delay(0)
-
-  expect(done).toBe(false)
-
-  count.value++
-  await delay(0)
-
-  expect(done).toBe(false)
-
-  count.value++
-  await delay(0)
-
-  expect(done).toBe(true)
+    expect(match.value).toBe(true)
+  })
 })
